ed8332487f0796b1f8f6a40033c3cec21967a751
chaimoma <elchaimae003@gmail.com>
2026-02-27 22:06:16 +0000

    Remove duplicate import statement in models.py

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, Column, Integer, String
-from app.database import Base
+from sqlalchemy import Column, Integer, String
+from .database import Base
 class User(Base):   
-
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     username = Column(String(50), unique=True, index=True)
